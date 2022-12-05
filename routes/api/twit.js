efc1fc9a1eba59c71d16e877ae61ef239b88d623
Desiholic <92688895+Desiholic@users.noreply.github.com>
2022-12-04 19:12:27 -0500

    Update twit.js

--- routes/api/twit.js
+++ routes/api/twit.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const twit = require('twit')
 
-const T = new twit({
+/*const T = new twit({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});*/
+
+const T = new twit({
+  consumer_key: VX9nVUYuyx4zbwuFrXA3D7xOZ,
+  consumer_secret: I1mFBdBqhrXuVPNH821qnw2HU8cBuTZr4nsbDgVZDpdDCjnWSy,
+  access_token: 1017110154490368003-yIeVyfTltdkkgGDNIXMG0Mtkvu0RAP,
+  access_token_secret: ID5rHrJIQgWaqk9dPHTmOprmfOhbsMzLkKOczalutdWNU,
 });
 
 module.exports = T;
